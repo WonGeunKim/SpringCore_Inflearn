51277964f9a47891e8bde2572fb63ec92aba6f59
WonGeunKim <kwg4764@naver.com>
2023-03-13 20:47:04 +0900

    - @Configuration과 Singleton

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,6 +15,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderSerivece -> new MemoryMemberRepository()
+
+    // 예상
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository
+
+    // 실제
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // =>  스프링이 싱글톤 보장
+
+
     // AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다
     // AppConfig는 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결)해준다
 
@@ -27,16 +44,22 @@ public class AppConfig {
     // 스프링 컨테이너에 등록
     @Bean
     public MemberService memberService() {
+
+       System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
@@ -48,6 +71,7 @@ public class AppConfig {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
+
 
 
 }
