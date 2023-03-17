9a7a7d0dc444fed537e5ba22859be827d022f23d
WonGeunKim <kwg4764@naver.com>
2023-03-17 20:33:21 +0900

    - 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)) // AppConfig를 빼기 위해서
+@ComponentScan(
+//        basePackages = "hello.core.member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+) // AppConfig를 빼기 위해서
 public class AutoAppConfig {
 
 
