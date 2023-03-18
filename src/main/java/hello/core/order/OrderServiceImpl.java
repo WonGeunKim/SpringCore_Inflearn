1bade6a5e364e3a08243c981e582b46f28c58fad
WonGeunKim <kwg4764@naver.com>
2023-03-18 19:45:43 +0900

    - Lombok

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor    // final이 붙은 필수값을 가지고 생성자를 만들어줌
 public class OrderServiceImpl implements OrderService{
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -28,14 +30,18 @@ public class OrderServiceImpl implements OrderService{
     @Autowired private DiscountPolicy discountPolicy;
     */
 
+
+/*
     // 생성자가 딱 1개만 있으면 @Autowired를 생략해도 자동 주입 된다. 물론 스프링 빈에만 해당한다.
     // @Autowired 를 사용하면 생성자에서 여러 의존관계도 한번에 주입받을 수 있다.
     // 생성자 주입
-    @Autowired
+//    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // 생성자를 통해서 할당
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+*/
+
 
     // 인터페이스에만 의존
 //    private DiscountPolicy discountPolicy;
