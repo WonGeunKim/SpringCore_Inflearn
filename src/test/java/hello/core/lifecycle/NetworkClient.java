9f396d94b8ed63b941dcbed31c97f87e70c401fd
WonGeunKim <kwg4764@naver.com>
2023-03-25 18:46:48 +0900

    - 빈 생명주기 인터페이스

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,15 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean , DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
 
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -30,4 +31,25 @@ public class NetworkClient {
         System.out.println("close : " + url);
     }
 
+
+    // <인터페이스
+    @Override
+    public void afterPropertiesSet() throws Exception {
+    // 의존관계 주입이 끝나면 호출해주겠다 라는 의미
+
+        System.out.println("NetworkClient.afterPropertiesSet");
+
+        connect();
+        call("초기화 연결 메시지");
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+        System.out.println("NetworkClient.destroy");
+
+        disconnect();
+    }
+    // 인터페이스>
 }
