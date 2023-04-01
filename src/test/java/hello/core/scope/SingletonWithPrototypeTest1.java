2f7e56989fda3f28059edfbe32533f0e379ee10c
WonGeunKim <kwg4764@naver.com>
2023-04-01 18:58:56 +0900

    - Provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,15 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -37,14 +40,17 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+//        assertThat(count2).isEqualTo(2);
+
+        assertThat(count2).isEqualTo(1);
     }
 
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
-
+//        private final PrototypeBean prototypeBean;  // 생성시점에 주입 x01
+        
+/*        
         @Autowired
         public ClientBean(PrototypeBean prototypeBean) {
             this.prototypeBean = prototypeBean;
@@ -53,6 +59,22 @@ public class SingletonWithPrototypeTest1 {
         public int logic() {
             prototypeBean.addCount();
 
+            int count = prototypeBean.getCount();
+            return count;
+        }
+        */
+
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+
+
+        public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
         }
